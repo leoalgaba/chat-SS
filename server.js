ecd132fc2216ceb76dc15fc9f95744651b2f4d4b
leoalgaba <leo_algaba@hotmail.com>
2017-03-21 10:47:48 +0100

    Agregando soporte para registro y login de usuarios. (MongoDB y bcrypt).
    añadiremos la capacidad de saber exactamente que usuarios están
    conectados a la aplicación en cualquier momento, también informaremos a
    los usuarios cada vez que alguien se desconecte.

--- server.js
+++ server.js
@@ -5,16 +5,82 @@
  */
 
 /* Librerias necesarias para la aplicación */
-var app  = require('express')();
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
-var io   = require('socket.io')(http);
+var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+var userDAO = require('./dao/UserDAO').UserDAO;
 
+// Para acceder a los parametros de las peticiones POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/* Mongodb config */
+var mdbconf = {
+  host: 'localhost',
+  port: '27017',
+  db: 'chatSS'
+};
+
+/* Get a mongodb connection and start application */
+MongoClient.connect('mongodb://'+mdbconf.host+':'+mdbconf.port+'/'+mdbconf.db, function (err, db) {
+
+  if (err) return new Error('Conexión a mongodb sin éxito');
+
+  var usersDAO = new userDAO(db); // Inicializar userDAO
+  var onlineUsers = [];
 
 /** *** *** ***
- *  Configuramos el sistema de ruteo para las peticiones web
- *  de manera que sin importar la ruta que el usuario solicite
- *  siempre lo direccionaremos al html del sistema de chat.
- */
+ *  Configuramos el sistema de ruteo para las peticiones web */
+  app.get('/signup', function (req, res) {
+    res.sendFile( __dirname + '/views/signup.html');
+  });
+
+  app.post('/signup', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email    = req.body.email;
+
+    usersDAO.addUser(username, password, email, function (err, user) {
+      if (err) {
+        res.send({ 'error': true, 'err': err});
+      }
+      else {
+        res.send({ 'error': false, 'user': user });
+      }
+    });
+  });
+
+  app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    usersDAO.validateLogin(username, password, function (err, user) {
+      if (err) {
+        res.send({'error': true, 'err': err});
+      }
+      else {
+        user.password = null;
+        res.send({ 'error': false, 'user': user});
+      }
+    });
+  });
+
+  /** css and js request */
+  app.get('/css/foundation.min.css', function (req, res) {
+    res.sendFile(__dirname + '/views/css/foundation.min.css');
+  });
+
+  app.get('/css/normalize.css', function (req, res) {
+    res.sendFile(__dirname + '/views/css/normalize.css');
+  });
+
+  app.get('/js/foundation.min.js', function (req, res) {
+    res.sendFile(__dirname + '/views/js/foundation.min.js');
+  });
+  /** *** *** */
 app.get('*', function(req, res) {
   res.sendFile( __dirname + '/views/chat.html');
 });
@@ -28,14 +94,20 @@ io.on('connection', function(socket) {
 
   console.log('Nuevo usuario conectado');
 
-  /**
-   * Cada nuevo socket debera estar a la escucha
+    /**
+     * Cada nuevo cliente solicita con este evento la lista
+     * de usuarios conectados en el momento.
+     */
+    socket.on('all online users', function () {
+      socket.emit('all online users', onlineUsers);
+    });
+
+  /**Cada nuevo socket debera estar a la escucha
    * del evento 'chat message', el cual se activa
    * cada vez que un usuario envia un mensaje.
    *
    * @param  msg : Los datos enviados desde el cliente a
-   *               través del socket.
-   */
+   * través del socket. */
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
@@ -44,16 +116,29 @@ io.on('connection', function(socket) {
    * Mostramos en consola cada vez que un usuario
    * se desconecte del sistema.
    */
-  socket.on('disconnect', function() {
-    console.log('Usuario desconectado');
-  });
+    socket.on('disconnect', function() {
+      onlineUsers.splice(onlineUsers.indexOf(socket.user), 1);
+      io.emit('remove user', socket.user);
+      console.log('Usuario desconectado');
+    });
 
-});
+    /**Cuando un cliente se conecta, emite este evento
+     * para informar al resto de usuarios que se ha conectado.
+     * @param  {[type]} nuser El nuevo usuarios
+     */
+    socket.on('new user', function (nuser) {
+      socket.user = nuser;
+      onlineUsers.push(nuser);
+      io.emit('new user', nuser);
+    });
+
+  });
 
 /**
  * Iniciamos la aplicación en el puerto 3000
  */
 http.listen(3000, function() {
   console.log('escuchando en puerto 3000');
+});
 });
 
